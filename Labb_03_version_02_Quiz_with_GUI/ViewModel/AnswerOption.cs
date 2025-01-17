20e2c2eec91303021f544e165adfd86b5cbd9cac
LuwiAnd <ludwigsn@gmail.com>
2025-01-17 03:26:19 +0100

    This is a partial update of the PlayerView to make it possible for the user to answer the questions, get a score and get feedback on whether they answered correctly before moving on to the next question. I need to fix the bindings from the buttons in PlayerView.xaml to the public ObservableCollection<AnswerOption> AnswerOptions { get; set; } in PlayerViewModel.cs. I believe that the AnswerOptions only have references to objects that does not change when when the elements of AnswerOptions are replaced, so I might need to find a way to update the elements instead of replacing them.

---
+++ Labb_03_version_02_Quiz_with_GUI/ViewModel/AnswerOption.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_03_version_02_Quiz_with_GUI.ViewModel
+{
+    internal class AnswerOption : ViewModelBase
+    {
+        private string _answerText;
+        public string AnswerText { 
+            get => _answerText;
+            set
+            {
+                _answerText = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private bool _isCorrect;
+        public bool IsCorrect
+        {
+            get => _isCorrect;
+            set
+            {
+                _isCorrect = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private Brush _backgroundColor;
+        public Brush BackgroundColor {
+            get => _backgroundColor;
+            set
+            {
+                _backgroundColor = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private int _optionIndex;
+        public int OptionIndex {
+            get => _optionIndex;
+            set
+            {
+                _optionIndex = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public AnswerOption(string answerText, bool isCorrect)
+        {
+            this.AnswerText = answerText;
+            this.IsCorrect = isCorrect;
+            this.BackgroundColor = Brushes.LightGray;
+        }
+    }
+}
--- Labb_03_version_02_Quiz_with_GUI/ViewModel/PlayerViewModel.cs
+++ Labb_03_version_02_Quiz_with_GUI/ViewModel/PlayerViewModel.cs
@@ -2,6 +2,10 @@
 using Labb_03_version_02_Quiz_with_GUI.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+//using System.Drawing;
+using System.Windows.Media;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +14,7 @@ using System.Windows.Threading;
 
 namespace Labb_03_version_02_Quiz_with_GUI.ViewModel
 {
+    enum QuizState { Asking, ShowingCorrectAnswer };
     internal class PlayerViewModel : ViewModelBase
     {
 
@@ -31,12 +36,18 @@ namespace Labb_03_version_02_Quiz_with_GUI.ViewModel
 
             //UpdateButtonCommand = new DelegateCommand(UpdateButton);
             UpdateButtonCommand = new DelegateCommand(UpdateButton, CanUpdateButton);
-            
-        }
 
+
+            AnswerCommand = new DelegateCommand(OnScoringQuestion);
+            QuizState QuizState = QuizState.Asking;
+            AnswerOptions = new ObservableCollection<AnswerOption>();
+            LoadAnswerOptions();
+        }
 
         private readonly MainWindowViewModel? mainWindowViewModel;
         private QuizCompletedViewModel quizCompletedViewModel;
+        
+        public QuizState QuizState { get; set; }
 
         public DelegateCommand StartQuizCommand { get; }
         public QuestionPackViewModel? ActivePack { get => mainWindowViewModel.ActivePack; }
@@ -174,19 +185,205 @@ namespace Labb_03_version_02_Quiz_with_GUI.ViewModel
         private void Timer_Tick(object? sender, EventArgs e)
         {
             //TestData += 'x';
-            if(SecondsRemainingToAnswer > 0)
+            if(QuizState == QuizState.Asking)
             {
-                SecondsRemainingToAnswer--;
-            }
-            else if(CurrentQuestionIndex < (ActivePack.Questions.Count - 1))
-            {
-                CurrentQuestionIndex++;
-                StartCurrentQuestion();
+
+                if(SecondsRemainingToAnswer > 0)
+                {
+                    SecondsRemainingToAnswer--;
+                }
+                else if(CurrentQuestionIndex < (ActivePack.Questions.Count - 1))
+                {
+                    //CurrentQuestionIndex++;
+                    //StartCurrentQuestion();
+                    QuizState = QuizState.ShowingCorrectAnswer;
+                    TimeToDisplayCorrectAnswerInSeconds = 3;
+                }
+                else
+                {
+                    mainWindowViewModel.ShowQuizCompletedView = true;
+                }
             }
             else
             {
-                mainWindowViewModel.ShowQuizCompletedView = true;
+                if(TimeToDisplayCorrectAnswerInSeconds <= 0)
+                {
+                    QuizState = QuizState.Asking;
+
+                    if(CurrentQuestionIndex < (ActivePack.Questions.Count - 1))
+                    {
+                        CurrentQuestionIndex++;
+                        StartCurrentQuestion();
+                    }
+                    else
+                    {
+                        mainWindowViewModel.ShowQuizCompletedView = true;
+                    }
+                }
+                TimeToDisplayCorrectAnswerInSeconds--;
             }
         }
+
+
+
+
+
+
+
+
+        // Code for handling button clicks.
+        public ObservableCollection<AnswerOption> AnswerOptions { get; set; }
+        private readonly Random random = new Random();
+
+        private int TimeToDisplayCorrectAnswerInSeconds = 3;
+        private string _selectedAnswer;
+        private int _correctAnswerIndex;
+        
+        public DelegateCommand AnswerCommand { get; }
+
+
+
+        private void LoadAnswerOptions()
+        {
+            AnswerOptions.Clear();
+
+            List<AnswerOption> options = new List<AnswerOption>
+            {
+                new AnswerOption(CurrentQuestion.CorrectAnswer, true)
+            };
+
+            options.AddRange(CurrentQuestion.IncorrectAnswers.Select(answer => new AnswerOption(answer, false)));
+
+            
+            List<AnswerOption> shuffledOptions = options.OrderBy(_ => random.Next()).ToList();
+
+            for( int i = 0; i < shuffledOptions.Count; i++)
+            {
+                shuffledOptions[i].OptionIndex = i;
+            }
+
+            AnswerOptions = new ObservableCollection<AnswerOption>(shuffledOptions);
+            
+        }
+
+
+        private void OnScoringQuestion(object? parameter)
+        {
+            var selectedOption = parameter as AnswerOption;
+
+            DisplayCorrectAnswerButton();
+
+            if(!selectedOption.IsCorrect)
+            {
+                AnswerOptions[selectedOption.OptionIndex].BackgroundColor = Brushes.Red;
+            }
+
+            TimeToDisplayCorrectAnswerInSeconds = 3;
+            QuizState = QuizState.ShowingCorrectAnswer;
+        }
+
+        private void DisplayCorrectAnswerButton()
+        {
+            var correctAnswer = AnswerOptions.FirstOrDefault(option => option.IsCorrect);
+            if(correctAnswer != null)
+            {
+                correctAnswer.BackgroundColor = Brushes.Green;
+            }
+        }
+
+
+
+        private void UpdateAnswerButtonColors(bool isCorrect, string selectedAnswer)
+        {
+            if (isCorrect)
+            {
+                if(AnswerOption1 == selectedAnswer) { Button1Color = Brushes.Green; }
+                if(AnswerOption2 == selectedAnswer) { Button2Color = Brushes.Green; }
+                if(AnswerOption3 == selectedAnswer) { Button3Color = Brushes.Green; }
+                if(AnswerOption4 == selectedAnswer) { Button4Color = Brushes.Green; }
+            }
+            else
+            {
+                if (AnswerOption1 == selectedAnswer) { Button1Color = Brushes.Red; }
+                if (AnswerOption2 == selectedAnswer) { Button2Color = Brushes.Red; }
+                if (AnswerOption3 == selectedAnswer) { Button3Color = Brushes.Red; }
+                if (AnswerOption4 == selectedAnswer) { Button4Color = Brushes.Red; }
+
+                if(AnswerOption1 == CurrentQuestion?.CorrectAnswer) { Button1Color = Brushes.Green; }
+                if(AnswerOption2 == CurrentQuestion?.CorrectAnswer) { Button2Color = Brushes.Green; }
+                if(AnswerOption3 == CurrentQuestion?.CorrectAnswer) { Button3Color = Brushes.Green; }
+                if(AnswerOption4 == CurrentQuestion?.CorrectAnswer) { Button4Color = Brushes.Green; }
+            }
+        }
+
+        private void ResetButtonColors()
+        {
+            Button1Color = Brushes.LightGray;
+            Button2Color = Brushes.LightGray;
+            Button3Color = Brushes.LightGray;
+            Button4Color = Brushes.LightGray;
+        }
+
+
+        private Brush _button1Color = Brushes.LightGray;
+        public Brush Button1Color
+        {
+            get => _button1Color;
+            set { _button1Color = value; RaisePropertyChanged(); }
+        }
+
+        private Brush _button2Color = Brushes.LightGray;
+        public Brush Button2Color
+        {
+            get => _button2Color;
+            set { _button2Color = value; RaisePropertyChanged(); }
+        }
+
+        private Brush _button3Color = Brushes.LightGray;
+        public Brush Button3Color
+        {
+            get => _button3Color;
+            set { _button3Color = value; RaisePropertyChanged(); }
+        }
+
+        private Brush _button4Color = Brushes.LightGray;
+        public Brush Button4Color
+        {
+            get => _button4Color;
+            set { _button4Color = value; RaisePropertyChanged(); }
+        }
+
+
+
+
+        private string _answerOption1;
+        public string AnswerOption1
+        {
+            get => _answerOption1;
+            set { _answerOption1 = value; RaisePropertyChanged(); }
+        }
+
+        private string _answerOption2;
+        public string AnswerOption2
+        {
+            get => _answerOption2;
+            set { _answerOption2 = value; RaisePropertyChanged(); }
+        }
+
+        private string _answerOption3;
+        public string AnswerOption3
+        {
+            get => _answerOption3;
+            set { _answerOption3 = value; RaisePropertyChanged(); }
+        }
+
+        private string _answerOption4;
+        public string AnswerOption4
+        {
+            get => _answerOption4;
+            set { _answerOption4 = value; RaisePropertyChanged(); }
+        }
+
+
     }
 }
